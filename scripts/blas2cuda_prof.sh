420116a20033854d133e758fdf7b2ca8c7220905
Princeton Ferro <princetonferro@gmail.com>
2017-12-10 15:06:08 -0500

    Add profiling script for blas2cuda.

---
+++ scripts/blas2cuda_prof.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# blas2cuda.sh
+# preload libblas2cuda.so and run with a tracker file
+
+trackfile=$1
+progname=$2
+BLAS2CUDA=../libblas2cuda.so
+
+if [ -z $trackfile ] || [ -z $progname ]; then
+    echo "Usage: $0 <objtrackfile> <command> ARGS..."
+    exit 1
+fi
+
+if [ ! -e $BLAS2CUDA ]; then
+    if ! make -C $(dirname $BLAS2CUDA); then
+        exit 1
+    fi
+fi
+
+ltrace -Tf env LD_PRELOAD=$BLAS2CUDA BLAS2CUDA_OPTIONS="track=$trackfile" $progname ${@:3}
