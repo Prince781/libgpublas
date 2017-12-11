4b241988345ca180468fe588e7b871d70a96e5eb
Princeton Ferro <princetonferro@gmail.com>
2017-12-10 21:16:09 -0500

    Run the object tracker in gdb.

---
+++ scripts/objtrack_dbg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# :vim tw=0
+# Run the object tracker through gdb
+
+pardir=$(dirname $(readlink -f $0))
+debugcmdfile=$1
+blas_lib=$2
+program=$3
+args=${@:4}
+fname=$(basename $program.objtrack)
+LIBOBJTRACKER=$pardir/../lib/libobjtracker.so
+
+
+if [ -z $debugcmdfile ] || [ -z $blas_lib ] || [ -z $program ]; then
+    printf "Usage: $0 <debugcmdfile> <blas_lib.so> <program> ARGS...\n"
+    exit 1
+fi
+
+if [ ! -e $LIBOBJTRACKER ]; then
+    make -C $(dirname $LIBOBJTRACKER) $(basename $LIBOBJTRACKER)
+fi
+
+tempfile=$(mktemp)
+echo "creating $tempfile"
+
+cat >$tempfile <<EOF
+set exec-wrapper env 'OBJTRACKER_BLASLIB=$blas_lib' 'LD_PRELOAD=$LIBOBJTRACKER'
+run $args
+$(cat $debugcmdfile)
+run $args
+EOF
+
+gdb $program -x $tempfile
+rm $tempfile
