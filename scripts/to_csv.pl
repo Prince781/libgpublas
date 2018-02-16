87f8e84347d42ab7dd0c44825c47fc65138416b7
Prince781 <princetonferro@gmail.com>
2018-02-16 10:47:47 -0500

    Changes.

--- scripts/to_csv.pl
+++ scripts/to_csv.pl
@@ -1,6 +1,7 @@
 #!/bin/perl
 use strict;
 use warnings;
+use POSIX ();
 
 my ($data, $csvsuffix) = @ARGV;
 
@@ -13,8 +14,14 @@ if (not defined $data or not defined $csvsuffix) {
     print_help_exit();
 }
 
-# name -> [results]
+# n -> [results]
 my %table;
+
+my ($suffix) = $data =~ m/.+\.(.+)\..+/;
+
+if (not defined $suffix) {
+    die "Suffix not defined!";
+}
 
 open (my $datafh, "<", "$data")
     or die "Could not open $data\n";
@@ -31,26 +38,57 @@ while (my $line = <$datafh>) {
         # write header
         # print $csvfh "Matrix Size, Time(s)\n";
 
-        # read every value 
-        while (my ($n, $seconds, $nanoseconds) = 
-            <$datafh> =~ m/[\s\w]*\w+\[n=\s*(\d+)\].* (\d+)\s*s\s*\+\s*(\d+)\s*ns/) {
-            my $time = $seconds + $nanoseconds / 10e+9;
+        if ($suffix ne "octave") {
+            # read every value 
+            while (my $line2 = <$datafh>) {
+                if (my ($n, $seconds, $nanoseconds) = 
+                    $line2 =~ m/[\s\w]*\w+\[n=\s*(\d+)\].* (\d+)\s*s\s*\+\s*(\d+)\s*ns/) {
+                    my $time = $seconds + $nanoseconds / 10e+9;
 
-            printf $csvfh "%d,%lf\n", $n, $time;
-        }
+                    printf $csvfh "%d,%lf\n", $n, $time;
+                    push @{$table{$n}}, $time;
+                } else {
+                    last;
+                }
+            }
+        } else {
+            while (my $line2 = <$datafh>) {
+                if (my ($n, $seconds, $gflops) =
+                    $line2 =~ m/[\s\w]*\w+\[n=\s*(\d+)\].* elapsed=(\d+\.\d+)\s*s\s*GFLOPS=\s*(\d+.\d+)\s*/) {
+                    my $time = $seconds;
 
-        # for octave results
-        while (my ($n, $seconds, $gflops) =
-            <$datafh> =~ m/[\s\w]*\w+\[n=\s*(\d+)\].* elapsed=(\d+\.\d+)\s*s\s*GFLOPS=\s*(\d+.\d+)\s*/) {
-            my $time = $seconds;
-
-            printf $csvfh "%d,%lf\n", $n, $time;
+                    printf $csvfh "%d,%lf\n", $n, $time;
+                    push @{$table{$n}}, $time;
+                } else {
+                    last;
+                }
+            }
         }
 
         close $csvfh;
     } else {
         print "Skipping line: '$line'\n";
     }
+}
+
+print "LaTeX output for $suffix:\n";
+foreach my $key (sort { $a <=> $b } keys %table) {
+    print "\$n=$key\$";
+    foreach my $val (@{$table{$key}}) {
+        my $js = "js52";
+        if (not -e "/bin/js52") {
+            $js = "js38";
+            if (not -e "/bin/js38") {
+                $js = "";
+            }
+        }
+        if ($js ne "") {
+            printf " & \$%.5f\$", `$js -e "print ($val.toPrecision(3));"`;
+        } else {
+            printf " & \$%.5f\$", $val;
+        }
+    }
+    print "\t\\\\ \n";
 }
 
 close $datafh;
