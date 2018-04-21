86049400e569ead61d9c03dafeb7dba74f9da382
Princeton Ferro <princetonferro@gmail.com>
2018-04-21 19:55:56 -0400

    Update script.

--- scripts/analyze_trace.py
+++ scripts/analyze_trace.py
@@ -14,8 +14,10 @@ numitems_host = 0
 totalsize_host = 0
 numitems_dev = 0
 totalsize_dev = 0
+lineno = 0
+failed_to_match = []
 
-class Event(object):
+class Event:
     def __init__(self, numhost, numdev, sizehost, sizedev):
         self.numhost = numhost
         self.numdev = numdev
@@ -25,7 +27,7 @@ class Event(object):
     def __str__(self):
         return f'Host[{self.numhost} items and {self.sizehost} B] Dev[{self.numdev} items and {self.sizedev} B]'
 
-class Record(object):
+class Record:
     def __init__(self, fun, reqsize, start, uid, is_gpu=False):
         self.fun = fun
         self.reqsize = reqsize
@@ -37,9 +39,10 @@ class Record(object):
         return f'{"D" if self.is_gpu else "H"} {self.uid} fun=[{self.fun}] reqsize=[{self.reqsize}] alive={self.alive}'
 
 for line in gzip.open(sys.argv[1], 'rt'):
-    m = re.match(r'([TUC]) fun=\[(\w+)\] reqsize=\[(\d+)\] tid=\[\d+\] time=\[(\d+)s\+(\d+)ns\] uid=\[(\d+)\]', line)
+    lineno = lineno + 1
+    m = re.match(r'([TUC]).*fun=\[(\w+)\] reqsize=\[(\d+)\] tid=\[(\d+)\] time=\[(\d+)s\+(\d+)ns\] uid=\[(\d+)\]', line)
     if m:
-        tp, fun, reqsize, time_s, time_ns, uid = m.group(1,2,3,4,5,6)
+        tp, fun, reqsize, time_s, time_ns, uid = m.group(1,2,3,5,6,7)
         time = int(int(time_s) * 10e9 + int(time_ns))
         if tp == 'T':
             numitems_host += 1
@@ -74,12 +77,18 @@ for line in gzip.open(sys.argv[1], 'rt'):
                         events[key].numdev += 1
                         events[key].sizehost -= records[int(uid)].reqsize
                         events[key].sizedev += records[int(uid)].reqsize
+    else:
+        failed_to_match.append(lineno)
+
+if failed_to_match:
+    with gzip.open('failed-to-match.txt.gz', 'wt') as f:
+        f.write(f'Failed to match {len(failed_to_match)} items from {sys.argv[1]}. Below are their line numbers:\n')
+        for lno in failed_to_match:
+            f.write(f'{lno}\n')
 
 for key in records:
     start, end = records[key].alive
     records[key].alive = (start - earliest, (end - earliest) if end else None)
-
-print(events)
 
 events = {int(key) - int(earliest) : val for key,val in events.items()}
 
