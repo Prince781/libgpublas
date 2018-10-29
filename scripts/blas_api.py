aea82e80ad5cfdb79e51789a156c4bb474f80823
Prince781 <princetonferro@gmail.com>
2018-10-28 20:06:33 -0400

    Moving BLAS API to separate file.

---
+++ scripts/blas_api.py
@@ -0,0 +1,7 @@
+arg_parsers = {\
+        'sgemm_': [ {'ptr': 6, 'nrows': 2, 'ncols': 4}, {'ptr': 8, 'nrows': 4, 'ncols': 3}, {'ptr': 11, 'nrows': 2, 'ncols': 3, 'output': True} ],
+        'cblas_sgemm': [ {'ptr': 7, 'nrows': 3, 'ncols': 5}, {'ptr': 9, 'nrows': 5, 'ncols': 4}, {'ptr': 12, 'nrows': 3, 'ncols': 4, 'output': True} ],
+        'dgemm_': [ {'ptr': 6, 'nrows': 2, 'ncols': 4}, {'ptr': 8, 'nrows': 4, 'ncols': 3}, {'ptr': 11, 'nrows': 2, 'ncols': 3, 'output': True} ],
+        'cblas_dgemm': [ {'ptr': 7, 'nrows': 3, 'ncols': 5}, {'ptr': 9, 'nrows': 5, 'ncols': 4}, {'ptr': 12, 'nrows': 3, 'ncols': 4, 'output': True} ],
+        }
+
--- scripts/dataflow_ltrace.py
+++ scripts/dataflow_ltrace.py
@@ -4,16 +4,10 @@
 import sys
 import re
 import gzip
+from blas_api import *
 
 num_nodes = 0
 node_nums = []
-
-arg_parsers = {\
-        'sgemm_': [ {'ptr': 6, 'nrows': 2, 'ncols': 4}, {'ptr': 8, 'nrows': 4, 'ncols': 3}, {'ptr': 11, 'nrows': 2, 'ncols': 3, 'output': True} ],
-        'cblas_sgemm': [ {'ptr': 7, 'nrows': 3, 'ncols': 5}, {'ptr': 9, 'nrows': 5, 'ncols': 4}, {'ptr': 12, 'nrows': 3, 'ncols': 4, 'output': True} ],
-        'dgemm_': [ {'ptr': 6, 'nrows': 2, 'ncols': 4}, {'ptr': 8, 'nrows': 4, 'ncols': 3}, {'ptr': 11, 'nrows': 2, 'ncols': 3, 'output': True} ],
-        'cblas_dgemm': [ {'ptr': 7, 'nrows': 3, 'ncols': 5}, {'ptr': 9, 'nrows': 5, 'ncols': 4}, {'ptr': 12, 'nrows': 3, 'ncols': 4, 'output': True} ],
-        }
 
 class Node:
     def __init__(self, name, inputs, outputs, optable, parents, on_blas_path):
