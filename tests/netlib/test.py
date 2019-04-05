55d5e585bd4e0aadaa651c456127dd72251d4419
Prince781 <princetonf@optonline.net>
2019-04-05 19:06:02 -0400

    Update tests.

--- tests/netlib/test.py
+++ tests/netlib/test.py
@@ -17,11 +17,30 @@ if __name__ == '__main__':
     args.input = os.path.abspath(args.input)
     args.binary = os.path.abspath(args.binary)
     args.gpublas = os.path.abspath(args.gpublas)
+    
+    retval = 0
 
+    # see https://www.tldp.org/LDP/abs/html/exitcodes.html
     subprocess.run(['rm', '-f', f'{args.test.upper()}.SUMM'], check=True)
     if args.nointeractive:
-        subprocess.run(['sh', '-c', f'env LD_PRELOAD={args.gpublas} {args.binary} <{args.input}'], check=True)
+        try:
+            subprocess.run(['sh', '-c', f'env LD_PRELOAD={args.gpublas} {args.binary} <{args.input}'], check=True)
+        except:
+            os._exit(126)
+        try:
+            grep = subprocess.run(['grep', '-Ei', '(illegal|[*]+)', f'{args.test.upper()}.SUMM'])
+            if not grep.returncode:
+                with open(f'{args.test.upper()}.SUMM', 'r') as f:
+                    sys.stderr.writelines(f.readlines())
+            retval = not grep.returncode
+
+        except:
+            os._exit(126)
     else:
-        subprocess.run(['gdb', args.binary, '-ex', f'set exec-wrapper env LD_PRELOAD={args.gpublas}', '-ex', f'run <{args.input}'], check=True)
-        subprocess.run(['edit', f'{args.test.upper()}.SUMM'], check=True)
+        try:
+            subprocess.run(['gdb', args.binary, '-ex', f'set exec-wrapper env LD_PRELOAD={args.gpublas}', '-ex', f'run <{args.input}'], check=True)
+            subprocess.run(['edit', f'{args.test.upper()}.SUMM'], check=True)
+        except:
+            os._exit(126)
     print(f'Done testing {args.test}')
+    os._exit(retval)
